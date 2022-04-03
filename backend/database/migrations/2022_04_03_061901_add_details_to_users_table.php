ffb0b5d80a25a66cd23376fb2e837c899cb371a8
Salvatore Dininni <s.dininni@yahoo.com>
2022-04-03 08:40:27 +0200

    implemented table user details

---
+++ backend/database/migrations/2022_04_03_061901_add_details_to_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('img')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('address')->nullable();
+            $table->text('description')->nullable();
+            $table->string('zip_code')->nullable();
+            $table->string('city')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            Schema::dropIfExists('users')->nullable();
+        });
+    }
+};
